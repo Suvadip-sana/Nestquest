71d4fa2d5b680b5c1a80d97171b6c21f99e94b59
Suvadip-sana <suvadipsana602@gmail.com>
2024-08-14 02:03:00 +0530

    Add Form validatior

--- models/listing.js
+++ models/listing.js
@@ -8,16 +8,28 @@ const listingSchema = new Schema({
         type: String,
         required: true,
     },
-    description: String,
+    description: {
+        type: String,
+        required:true,
+    },
     image: {
         type: String,
         default: "https://images.unsplash.com/photo-1519046904884-53103b34b206?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set: (v) => v === "" ? "https://images.unsplash.com/photo-1519046904884-53103b34b206?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
         // Set for set a default value into this in case user does't provide any image URL
     },
-    price: Number,
-    location: String,
-    country: String
+    price: {
+        type: Number,
+        required: true,
+    },
+    location:{
+        type: String,
+        // required: true,
+    },
+    country: {
+        type: String,
+        // required: true,
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
