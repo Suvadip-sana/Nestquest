990902e7c483231806b0f9fd4955e0f9dbaf978d
Suvadip-sana <suvadipsana602@gmail.com>
2024-08-26 18:42:35 +0530

    Updated Data

--- init/index.js
+++ init/index.js
@@ -16,6 +16,7 @@ main() .then(() => {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj, owner: "66c63d6f76d4d759e5e84620"})); // inside initData access the data array and apply the map function, For every object add this function a owner object id along with other property(...obj).
     await Listing.insertMany(initData.data);
     console.log("Data was saved!");
 };
