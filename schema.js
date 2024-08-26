62604afa3d9eab54de1910baf791d149cfbaacb0
Suvadip-sana <suvadipsana602@gmail.com>
2024-08-26 18:55:30 +0530

    Update --> review schema validation with Joi

--- schema.js
+++ schema.js
@@ -2,11 +2,12 @@
 
 
 const Joi = require('joi'); // Requireed joi
+// const review = require('./models/review');
 // const Listing = require('./models/listing');
 
 
 
-// now define a scema with the help of joi
+// Now define a scema with the help of joi
 module.exports.listingSchema = Joi.object({ // Means inside Joi always required a object that is supposed to be 'listing'
     listing: Joi.object({ // Acording to joi this listing is always be a object
         title: Joi.string().required(),
@@ -16,6 +17,16 @@ module.exports.listingSchema = Joi.object({ // Means inside Joi always required 
         country: Joi.string().required(),
         image: Joi.string().allow("", null), // Alow the image empty or null because by default mongoose set a default value if empty
     }).required(), // this listing object always be required
+});
+
+
+
+// Define the review schema for validation with Joi
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string().required()
+    }).required(),
 });
 
 
