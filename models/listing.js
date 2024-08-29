67913010e130a009a0e5488dfdf1988867e5d279
Suvadip-sana <suvadipsana602@gmail.com>
2024-08-30 02:00:34 +0530

    Add a new field for storing geometry in GeoJSON format

--- models/listing.js
+++ models/listing.js
@@ -32,6 +32,17 @@ const listingSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "User",
     },
+    geometry: { // Add new key/field to store location coordinate in GeoJSON format
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            required: true
+          },
+          coordinates: {
+            type: [Number],
+            required: true
+          },
+    },
 });
 
 
