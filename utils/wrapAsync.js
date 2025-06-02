a95d1d3a0941314753e760726398e35d0d857a3a
Suvadip-sana <suvadipsana602@gmail.com>
2025-06-03 00:52:05 +0530

    Update 2.o

--- utils/wrapAsync.js
+++ utils/wrapAsync.js
@@ -1,5 +1,5 @@
 module.exports = (fn) => {
     return (req, res, next) => {
-        fn(req, res, next).catch(next);
+        fn(req, res, next).catch(next); //Since 'fn' is an asyncronous function so it return a promish
     };
 };
